979ee797faf382428dab69e07bde799ed1e0f3cf
Abd453 <134734720+Abd453@users.noreply.github.com>
2025-04-09 21:32:29 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (86.27%) - LeetSync

---
+++ 2491-smallest-even-multiple/smallest-even-multiple.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        if n % 2 == 0:
+            return n
+        elif n % 2 != 0:
+            return n*2
+       
+
+
+
+ 
